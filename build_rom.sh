b9896241d159aa1a53699af6fd5c5c6a7cf4d7c7
Orel Yosupov <85583144+Orel6505@users.noreply.github.com>
2022-05-07 14:52:48 +0300

    temporary: fix few mistakes

--- build_rom.sh
+++ build_rom.sh
@@ -1,21 +1,13 @@
 # sync rom
-
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b 12.1 11 -g default,-mips,-darwin,-notdefault
-
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Krishdangi/local_manifests/ --depth 1 -b main .repo/local_manifests
-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-
 source build/envsetup.sh
-
-lunch lineage_rosemary-userdebug
-
+lunch aosp_rosemary-userdebug
 export TZ=Asia/Dhaka #put before last build command
-
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
